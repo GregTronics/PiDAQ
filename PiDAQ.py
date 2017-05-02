52161a8f44c924eb2eeda73bc11e182be46387bc
GregTronics <gregtronics87@gmail.com>
2017-05-02 11:02:24 -0400

    Added print statement in autodetect for debugging.

--- PiDAQ.py
+++ PiDAQ.py
@@ -550,12 +550,15 @@ def autoDetectSensors():
 	for sensors in getThermoEnabled():
 		print tcStart + thermoAddress[sensors] + tcEnd
 		lines = temp_raw(tcStart + thermoAddress[sensors] + tcEnd)
+		print lines
 		while lines[0].strip()[-3:] != 'YES':
 			time.sleep(0.2)
 			lines = temp_raw(tcStart + thermoAddress[sensors] + tcEnd)
+			print lines
 		temp_output = lines[1].find('t=')
 		if temp_output != -1:
 			temp_string = lines[1].strip()[temp_output+2]
+			print temp_string
 			temp_c = float(temp_string) / 1000.0
 			temp_f = temp_c * 9.0 / 5.0 + 32.0
 			print temp_c, temp_f
